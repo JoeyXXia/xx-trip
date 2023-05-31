609c541f263923c65ed1e3deaabe86f7dc11568b
Xu <whitenight.xiaxu@gmail.com>
2023-05-31 23:51:45 +0800

    add router & pinia

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-<h1>hello</h1>
+<div class="app">
+    <router-view/>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/favor">收藏</router-link>
+    <router-link to="/order">订单</router-link>
+    <router-link to="/message">消息</router-link>
+</div>
 </template>
 
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import pinia from './store'
 
 import "normalize.css"
+import "./assets/css/index.css"
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
