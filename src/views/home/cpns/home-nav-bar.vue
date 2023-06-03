c096781cc3a27faf1d003769fc2b614def20f9a1
Xu <whitenight.xiaxu@gmail.com>
2023-06-03 16:33:39 +0800

    home navbar

---
+++ src/views/home/cpns/home-nav-bar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="nav-bar">
+        <div class="title">XX旅途</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+    .nav-bar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 47px;
+        border-bottom: 1px solid #ccc;
+
+    .title{
+        font-size: 20px;
+        font-weight: bold;
+        color: var(--primary-color);
+        line-height: 16x;
+        text-align: center;  
+    }
+
+    } 
+    
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,47 @@
 <template>
     <div class="home">
-        <h2>home</h2>
-    </div>
+        <home-nav-bar/>
+        <div class="banner">
+            <img src="@/assets/img/home/banner.webp" alt="">
+        </div>
+        <div class="location">
+            <div class="city">上海</div>
+            <span class="position">我的位置</span>
+            <img src="@/assets/img/home/icon_location.png" alt="">
+        </div>
+        </div>
 </template>
 
 <script setup>
 
+import HomeNavBar from './cpns/home-nav-bar.vue'
 </script>
 
 <style lang="less" scoped>
 
+    .banner 
+    img {
+        width: 100%;
+    }
+    .location {
+        display: flex;
+        align-items: center;
+        height: 43px;
+        padding: 0 20px;
+        .city {
+            flex: 1;
+        }
+
+        .position {
+            width: 74px;
+            display: flex;
+            align-items: center;
+
+            img {
+                margin-left: 5px;
+                width: 16px;
+                height: 16px;
+            }
+        }
+    }
 </style>
