289b7d556398f840f04ae3d6c93f484a4549c093
Xu <whitenight.xiaxu@gmail.com>
2023-06-03 14:57:52 +0800

    tabbar router

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="tab-bar">
         <template v-for="(item,index) in tabbarData">
-            <div class="tab-bar-item">
-            <img :src="getAssetURL(item.image)" alt="">
+            <div 
+            class="tab-bar-item" 
+            :class="{active: currentIndex === index}"
+            @click="itemClickk(index,item)"
+            >
+            <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
+            <img v-else :src="getAssetURL(item.imageActive)" alt="">
             <span class="text">{{ item.text }}</span> 
         </div>
         </template>
@@ -12,6 +17,15 @@
 <script setup>
 import tabbarData from '@/assets/data/tabbar.js'
 import { getAssetURL } from '@/utils/load_assets.js'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const currentIndex = ref(0)
+const router = useRouter()
+const itemClickk = (index,item) => {
+    currentIndex.value = index
+    router.push(item.path)
+}
 
 </script>
 
@@ -34,6 +48,9 @@ import { getAssetURL } from '@/utils/load_assets.js'
             justify-content: center;
             align-items: center;
 
+            &.active {
+                color: var(--primary-color);
+            }
             .text  {
                 font-size: 12px;
             }
