d8b3fcf3cd7588b9b7f0a3e1394a0b12603246dd
Xu <whitenight.xiaxu@gmail.com>
2023-06-07 22:07:02 +0800

    city tab

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -8,6 +8,12 @@ import { ref } from 'vue';
         show-action
         @cancel="cancelClick"
         />
+
+    <van-tabs v-model:active="tabActive" color="#ff9854">
+        <van-tab title="国内·港澳台"></van-tab>
+        <van-tab title="海外"></van-tab>
+    </van-tabs>
+
     </div>
 </template>
 
@@ -15,11 +21,15 @@ import { ref } from 'vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
+//search
 const searchValue = ref("")
 const router = useRouter()
 const cancelClick =  () => {
     router.back()
 }
+
+//tan
+const tabActive = ref()
 
 </script>
 
