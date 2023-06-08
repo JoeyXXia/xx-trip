4518085c6809dcd4d499a12fa99c636f3c1dfc8d
Xu <whitenight.xiaxu@gmail.com>
2023-06-08 22:38:19 +0800

    group data

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,7 +1,7 @@
-import { ref } from 'vue';
 <template>
-    <div class="city">
-        <van-search 
+    <div class="city top-page">
+        <div class="top">
+            <van-search 
         v-model="searchValue" 
         placeholder="城市/区域/位置"  
         shape="round"
@@ -9,11 +9,20 @@ import { ref } from 'vue';
         @cancel="cancelClick"
         />
 
-    <van-tabs v-model:active="tabActive" color="#ff9854">
+    <van-tabs v-model:active="tabActive" color="#ff9854" line-height="3">
         <template v-for="(value,key,index) in allCities" :key= "key">
-            <van-tab :title="value.title"></van-tab>
+            <van-tab :title="value.title" :name="key"></van-tab>
         </template>
     </van-tabs>
+        </div>
+
+    <div class="content">
+        <template v-for="item in currentGroup?.cities">
+            <div >
+                {{ item }}
+            </div>
+        </template>
+    </div>
 
     </div>
 </template>
@@ -21,9 +30,9 @@ import { ref } from 'vue';
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-import {getCityAll} from "@/services"
 import  useCityStore from '@/stores/modules/city'
 import { storeToRefs } from 'pinia';
+import { computed } from '@vue/reactivity';
 
 //search
 const searchValue = ref("")
@@ -35,19 +44,26 @@ const cancelClick =  () => {
 //tab
 const tabActive = ref()
 
-// network request
-// const allCity= ref({})
-// getCityAll().then( res=> {
-//     allCity.value = res.data
-// })
 
 // store  city data
 const cityStore = useCityStore()
 cityStore.fetchAllCitiesData()
 const { allCities} = storeToRefs(cityStore)
 
+const currentGroup = computed( () => allCities.value[tabActive.value])
+
 </script>
 
 <style lang="less" scoped>
+    .city {
 
+        .top {
+            position: relative;
+            z-index: 9;
+        }
+        .content {
+            height: calc(100vh - 98px);
+            overflow-y: auto;
+        }
+    }
 </style>
