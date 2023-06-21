7a46e106e4b25b3fc57a4e358bd6a035752e0377
Xu <whitenight.xiaxu@gmail.com>
2023-06-21 16:33:06 +0800

    current date

--- src/views/home/cpns/home-search-box.vue
+++ src/views/home/cpns/home-search-box.vue
@@ -7,6 +7,24 @@
             <img src="@/assets/img/home/icon_location.png" alt="">
             </div>
         </div>
+
+        <div class="section date-range">
+            <div class="start">
+                <div class="date">
+                    <span class="tip">入住</span>
+                    <span class="time">{{startDate}}</span>
+                </div>
+            </div>
+            <div class="stay">
+                <span>1 day</span>
+            </div>
+            <div class="end">
+                <div class="date">
+                    <span class="tip">离店</span>
+                    <span class="time">{{endDate}}</span>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,6 +32,8 @@
 import { useRouter } from 'vue-router';
 import useCityStore from '@/stores/modules/city';
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+import {formatMonthDay} from '@/utils/format_date'
 
 const router = useRouter();
 
@@ -35,11 +55,15 @@ const cityClick = () => {
 // get current city
 const cityStore = useCityStore()
 const { currentCity } = storeToRefs(cityStore)
+
+// select date
+const nowDate = new Date()
+const startDate = ref(formatMonthDay(nowDate))
+const newDate = nowDate.setDate(nowDate.getDate() + 1)
+const endDate = ref(formatMonthDay(newDate))
 </script>
 
 <style lang="less" scoped>
-    .search-box {
-
         .location {
         display: flex;
         align-items: center;
@@ -47,6 +71,8 @@ const { currentCity } = storeToRefs(cityStore)
         padding: 0 20px;
         .city {
             flex: 1;
+            color: #333;
+            font-size: 15px;
         }
 
         .position {
@@ -55,6 +81,8 @@ const { currentCity } = storeToRefs(cityStore)
             align-items: center;
 
             .text {
+                position: relative;
+                top: 2px;
                 font-size: 12px;
                 margin-right: 3px;
                 
@@ -67,5 +95,51 @@ const { currentCity } = storeToRefs(cityStore)
             }
         }
     }
-    }
+        .section {
+            display: flex;
+            flex-wrap: wrap;
+            align-items: center;
+            padding: 0 20px;
+            color: #999;
+            height: 44px;
+
+            .start {
+                flex: 1;
+                display: flex;
+                height: 44px;
+                align-items: center;
+            }
+
+            .end {
+                min-width: 30%;
+                padding-left: 20px;
+            }
+
+            .date {
+                display: flex;
+                flex-direction: column;
+
+                .tip {
+                    font-size: 12px;
+                    color: #999;
+                }
+
+                .time {
+                    margin-top: 3px;
+                    color: #333;
+                    font-size: 15px;
+                    font-weight: 500;
+                }
+            }
+        }
+
+        .date-range {
+            .stay {
+                flex: 1;
+                text-align: center;
+                font-size: 12px;
+                color: #666;
+            }
+        }
+
 </style>
