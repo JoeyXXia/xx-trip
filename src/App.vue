a1bbcb45c98e24d19787dcd29ac40b94876f7ea4
Xu <whitenight.xiaxu@gmail.com>
2023-07-03 16:37:41 +0800

    request loading

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
 <div class="app">
     <router-view/>
-
     <tab-bar  v-if="!route.meta.hiddenTabbar"/>
+    <loading/>
 </div>
 </template>
 
 
 <script setup>
 import TabBar from '@/components/tab-bar/tab-bar.vue'
+import Loading from '@/components/loading/loading.vue'
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
--- src/services/request/index.js
+++ src/services/request/index.js
@@ -1,11 +1,29 @@
 import axios from 'axios'
 import { BASE_URL,TIME_OUT } from './config'
+import useMainStore from '@/stores/modules/main'
 
+
+const mainStorre = useMainStore()
 class XXRequest {
   constructor(baseURL, timeout=10000) {
     this.instance = axios.create({
       baseURL,
       timeout
+    })
+
+    this.instance.interceptors.request.use(config  => {
+      mainStorre.isLoading = true
+      return config
+    }, err => {
+      return err
+    })
+
+    this.instance.interceptors.response.use(res => {
+      mainStorre.isLoading = false
+      return res
+    },err => {
+      mainStorre.isLoading =  false
+      return err
     })
   }
 
