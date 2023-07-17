0489f6785ee4827cfdb11f066f2b778259ba6cbf
Xu <whitenight.xiaxu@gmail.com>
2023-07-17 16:20:07 +0800

    feat detail notice

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,48 +1,60 @@
 <template>
-    <div class="detail top-page">
-        <van-nav-bar
-        title="房屋详情"
-        left-text="旅途"
-        left-arrow
-        @click-left="onClickLeft"
-        />
-        <div class="main" v-if="mainPart">
-            <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics"/>
-            <detail-infos :top-infos="mainPart.topModule"/>
-            <detail-facility name="facility" :house-facility="mainPart.dynamicModule.facilityModule.houseFacility"/>
-            <detail-landlord name="landlord" :landlord="mainPart.dynamicModule.landlordModule"/>
-            <detail-comment name="comment" :comment="mainPart.dynamicModule.commentModule"/>
-        </div>
+  <div class="detail top-page">
+    <van-nav-bar
+      title="房屋详情"
+      left-text="旅途"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+    <div class="main" v-if="mainPart">
+      <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics" />
+      <detail-infos :top-infos="mainPart.topModule" />
+      <detail-facility
+        name="facility"
+        :house-facility="mainPart.dynamicModule.facilityModule.houseFacility"
+      />
+      <detail-landlord
+        name="landlord"
+        :landlord="mainPart.dynamicModule.landlordModule"
+      />
+      <detail-comment
+        name="comment"
+        :comment="mainPart.dynamicModule.commentModule"
+      />
+      <detail-notice
+        name="notice"
+        :order-rules="mainPart.dynamicModule.rulesModule.orderRules"
+      />
     </div>
+  </div>
 </template>
 
 <script setup>
-import { useRoute, useRouter } from 'vue-router';
-import { getDetailInfos } from "@/services"
-import { ref, computed } from 'vue';
+import { useRoute, useRouter } from "vue-router";
+import { getDetailInfos } from "@/services";
+import { ref, computed } from "vue";
 
-import DetailSwipe from "./cpns/detail_01-swipe.vue"
-import DetailInfos from "./cpns/detail_02-infos.vue"
-import DetailFacility from './cpns/detail_03-facility.vue'
-import DetailLandlord from './cpns/detail_04-landlord.vue'
-import DetailComment from './cpns/detail_05-comment.vue'
+import DetailSwipe from "./cpns/detail_01-swipe.vue";
+import DetailInfos from "./cpns/detail_02-infos.vue";
+import DetailFacility from "./cpns/detail_03-facility.vue";
+import DetailLandlord from "./cpns/detail_04-landlord.vue";
+import DetailComment from "./cpns/detail_05-comment.vue";
+import DetailNotice from "./cpns/detail_06-notice.vue";
 
-const router = useRouter()
-const route = useRoute()
-const houseId = route.params.id
+const router = useRouter();
+const route = useRoute();
+const houseId = route.params.id;
 
 const onClickLeft = () => {
-    router.back()
-}
+  router.back();
+};
 
-const detailInfos = ref({})
-const mainPart = computed( () => detailInfos.value.mainPart)
+const detailInfos = ref({});
+const mainPart = computed(() => detailInfos.value.mainPart);
 // sending netowrk request
-getDetailInfos(houseId).then( res => {
-    detailInfos.value = res.data
-})
+getDetailInfos(houseId).then((res) => {
+  detailInfos.value = res.data;
+});
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
