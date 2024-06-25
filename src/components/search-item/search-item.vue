504251c1e741ea336dc589c0f917f5bb9e71f322
Xu <whitenight.xiaxu@gmail.com>
2024-06-25 14:37:56 +0800

    search layout

---
+++ src/components/search-item/search-item.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search-item">
+    <div class="profile">
+      <div class="avatar">我</div>
+      <div class="like">喜欢</div>
+      <div class="comment">评论</div>
+    </div>
+    <div class="rating">4分，配备有洗衣机和冰箱</div>
+    <div class="position">
+      <span>郑家广场西门，整套1局一场2人</span>
+      <span>郑家服饰大床房广场开始建立和</span>
+    </div>
+    <div class="discount">
+      <div class="items">
+        <div class="item">早定早优惠98，</div>
+        <div class="item">立即确认</div>
+      </div>
+      <div class="price">
+        306
+        <span>8.0折扣</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
--- src/views/search/cpns/search-content.vue
+++ src/views/search/cpns/search-content.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="search-content">
-    <h2>search-content</h2>
+    <h2>search-header</h2>
+    <search-item />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import SearchItem from "@/components/search-item/search-item.vue"
+</script>
 
 <style lang="less" scoped></style>
