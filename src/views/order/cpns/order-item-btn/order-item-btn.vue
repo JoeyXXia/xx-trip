399f03ef74feeb109f67d96a7f1421d76f6504c4
Xu <whitenight.xiaxu@gmail.com>
2024-12-30 15:25:34 +0800

    order page layout

---
+++ src/views/order/cpns/order-item-btn/order-item-btn.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="order-item-btn">
+    <h2>order-item-btn"></h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
---
+++ src/views/order/cpns/order-item-info/order-item-info.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="order-item-info">
+    <h2>order-item-info"></h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
