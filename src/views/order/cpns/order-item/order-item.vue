dc3f7e43b2af31f9c5459eadaad364fc5201ab8f
Xu <whitenight.xiaxu@gmail.com>
2025-01-06 15:25:41 +0800

    organize order item data

--- src/views/order/cpns/order-item/order-item.vue
+++ src/views/order/cpns/order-item/order-item.vue
@@ -10,12 +10,45 @@
 import OrderItemTitle from "../order-item-title/order-item-title.vue"
 import OrderItemInfo from "../order-item-info/order-item-info.vue"
 import OrderItemBtn from "../order-item-btn/order-item-btn.vue"
+import { computed } from "vue"
 
 const props = defineProps({
   itemData: {
     type: Object,
     default: () => {},
   },
+})
+
+const getTitileData = computed(() => {
+  let data = props.itemData
+  return {
+    unitName: data.unitName,
+    orderStatusDesc: data.orderStatusDesc,
+    cancelReasonDesc: data.cancelReasonDesc,
+    orderStatus: data.orderStatus,
+    orderStatusDescColor: data.orderStatusDescColor,
+  }
+})
+
+const getInfoData = computed(() => {
+  let data = props.itemData
+  return {
+    checkInDate: data.checkInDate,
+    checkOutDate: data.checkOutDate,
+    checkInLatestTime: data.checkInLatestTime,
+    checkOutLatestTime: data.checkOutLatestTime,
+    prepayAmount: data.prepayAmount,
+    unitPicture: data.unitPicture,
+  }
+})
+
+const getBtnData = computed(() => {
+  let data = props.itemData
+  return {
+    countdown: data.countdown,
+    orderStatusDesc: data.orderStatusDesc,
+    orderStatus: data.orderStatus,
+  }
 })
 </script>
 
